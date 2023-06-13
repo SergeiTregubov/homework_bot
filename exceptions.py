f7a23ec75b1f8001b95c8dbab74de2f2a57af976
Your Name <you@example.com>
2023-06-13 14:37:13 +0300

    Readme

--- exceptions.py
+++ exceptions.py
@@ -2,16 +2,13 @@ class DateStampError(Exception):
     """Список домашних работ пуст"""
     pass
 
-
 class ResponseCodeError(Exception):
     """Статус ответа при запросе к API Яндекс Практикум отличается от 200"""
     pass
 
-
 class AvailabilityError(Exception):
     """Ошибка доступности переменных окружения"""
     pass
-
 
 class NoMessageError(Exception):
     """Ошибка отправки сообщения в телеграмм"""
